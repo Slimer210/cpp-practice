a4c151cb57ad23ec7644118fbaf7224bf0b8fd17
Slimer <66945259+Slimer210@users.noreply.github.com>
2022-07-18 08:33:08 +0800

    Update Sum of All Factors of N.cpp

--- competition code/Sum of All Factors of N.cpp
+++ competition code/Sum of All Factors of N.cpp
@@ -5,6 +5,7 @@ int main() {
     int n;
     cin >> n;
     int sum = 0;
+    // There's no factors that bigger than half of N, so this is why you can just validate half of the process.
     for (int i = 1; i <= n/2; i++) {
         if (n % i == 0) {
             sum += i;
